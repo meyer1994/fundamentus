b1a128838fb8742e29a8c4f1f7820e67ce769cf6
Joao Meyer <1994meyer@gmail.com>
2021-02-17 19:35:33 -0300

    Date instead of datetime

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import json
 from pathlib import Path
-from datetime import datetime
+from datetime import date
 from argparse import ArgumentParser
 
 import fundamentus
 
 parser = ArgumentParser()
-parser.add_argument('date', type=datetime.fromisoformat)
+parser.add_argument('date', type=date.fromisoformat)
 parser.add_argument('output', type=Path)
 args = parser.parse_args()
 
@@ -18,7 +18,7 @@ for key, val in data.items():
     ticker = args.output / key
     ticker.mkdir(parents=True, exist_ok=True)
 
-    filename = args.date.isoformat().replace(':', '-') + '.json'
+    filename = args.date.isoformat() + '.json'
     filename = ticker / filename
 
     with open(filename, 'wt') as file:
